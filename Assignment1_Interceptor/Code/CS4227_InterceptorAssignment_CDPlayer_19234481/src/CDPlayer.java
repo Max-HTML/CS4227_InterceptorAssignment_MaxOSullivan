8c40ec7fba270e7deb7d792d5c3488138ffcee93
Max-HTML <57290732+Max-HTML@users.noreply.github.com>
2023-02-24 15:30:34 +0000

    Add files via upload

--- Assignment1_Interceptor/Code/CS4227_InterceptorAssignment_CDPlayer_19234481/src/CDPlayer.java
+++ Assignment1_Interceptor/Code/CS4227_InterceptorAssignment_CDPlayer_19234481/src/CDPlayer.java
@@ -3,7 +3,14 @@
 //Course: LM121 Computer Systems
 //Module: CS4227 Software Design and Architecture
 
-public class CDPlayer {
+interface CDPlayerButtons{
+    public void play();
+    public void pause();
+    public void stop();
+
+}
+
+public class CDPlayer implements CDPlayerButtons {
     private boolean cdInserted = false;
     private String cdName = "";
     private String cdLength;
@@ -15,33 +22,48 @@ public class CDPlayer {
         cdName = cdInsertedName;
         cdLength = cdInsertedLength;
     }
+
     public void extractCD() {
         cdInserted = false;
     }
+
     public void play() {
         playing = true;
         paused = false;
     }
+
     public void pause() {
         playing = false;
         paused = true;
     }
-    public String getCDName(){
-        if(cdInserted != true) {
+
+    public void stop() {
+        playing = false;
+    }
+
+    public String getCDName() {
+        if (cdInserted != true) {
             return "Error: Insert CD!";
-        }
-        else {
+        } else {
             return cdName;
         }
 
     }
-    public String getCDLength(){
-        if(cdInserted != true) {
+
+    public String getCDLength() {
+        if (cdInserted != true) {
             return "Error: Insert CD!";
-        }
-        else {
+        } else {
 
             return cdLength;
+        }
+    }
+}
+
+    class main {
+        public static void main(String[] args) {
+            CDPlayer currentCDPlayer = new CDPlayer();  // Creating a CDPlayer Object
+            currentCDPlayer.getCDName();
         }
     }
 
@@ -50,5 +72,3 @@ public class CDPlayer {
 
 
 
-
-}
